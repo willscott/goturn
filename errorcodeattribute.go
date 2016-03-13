3eedb64b216f2e216c259918dc627907cdc6e256
Will Scott <willscott@gmail.com>
2016-03-13 14:12:25 -0700

    a couple more attributes

---
+++ errorcodeattribute.go
@@ -0,0 +1,54 @@
+package turn
+
+import (
+  "bytes"
+  "encoding/binary"
+  "errors"
+)
+
+type ErrorCodeAttribute struct {
+  Class uint8
+  Number uint8
+  Phrase string
+}
+
+func (h ErrorCodeAttribute) Type() (StunAttributeType) {
+  return ErrorCode
+}
+
+func (h ErrorCodeAttribute) Encode() ([]byte, error) {
+  buf := new(bytes.Buffer)
+  err := binary.Write(buf, binary.BigEndian, attributeHeader(StunAttribute(h)))
+  err = binary.Write(buf, binary.BigEndian, uint16(0))
+  err = binary.Write(buf, binary.BigEndian, h.Class)
+  err = binary.Write(buf, binary.BigEndian, h.Number)
+  err = binary.Write(buf, binary.BigEndian, h.Phrase)
+
+  if err != nil {
+    return nil, err
+  }
+  return buf.Bytes(), nil
+}
+
+func (h ErrorCodeAttribute) Decode(data []byte, length uint16) (error) {
+  if len(data) < 4 || len(data) < length {
+    return errors.New("Truncated Error Code Attribute")
+  }
+  if uint16(data[0:1]) != 0 {
+    return errors.New("Invalid reserved bytes in Error Code Attribute")
+  }
+  h.Class = uint8(data[2])
+  if h.Class < 3 || h.Class > 6 {
+    return errors.New("Invalid Error Code Class")
+  }
+  h.Number = uint8(data[3])
+  if h.Number > 99 {
+    return errors.New("Invlaid Error Code Number")
+  }
+  h.Phrase = string(data[4:length])
+  return nil
+}
+
+func (h ErrorCodeAttribute) Length() (uint16) {
+  return uint16(4 + len(h.Phrase))
+}
---
+++ fingerprintattribute.go
@@ -0,0 +1,38 @@
+package turn
+
+import (
+  "bytes"
+  "encoding/binary"
+  "errors"
+)
+
+type FingerprintAttribute struct {
+  CRC []byte
+}
+
+func (h FingerprintAttribute) Type() (StunAttributeType) {
+  return Fingerprint
+}
+
+func (h FingerprintAttribute) Encode() ([]byte, error) {
+  buf := new(bytes.Buffer)
+  err := binary.Write(buf, binary.BigEndian, attributeHeader(StunAttribute(h)))
+  err = binary.Write(buf, binary.BigEndian, h.CRC)
+
+  if err != nil {
+    return nil, err
+  }
+  return buf.Bytes(), nil
+}
+
+func (h FingerprintAttribute) Decode(data []byte, length uint16) (error) {
+  if length != 4 || len(data) < 4 {
+    return errors.New("Truncated Fingerprint Attribute")
+  }
+  h.CRC = data[0:4]
+  return nil
+}
+
+func (h FingerprintAttribute) Length() (uint16) {
+  return 4
+}
